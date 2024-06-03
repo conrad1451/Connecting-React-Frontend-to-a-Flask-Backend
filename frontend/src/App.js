4ae59d490ae3334851e8f1de493cce96c3b81e33
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-06-03 02:57:22 -0400

    Triggering re-deploy
    For getting environmental variable tested.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { useState,useEffect } from 'react'
 import './App.css';
 import ArticleList from './Components/ArticleList'
-
+ 
 function App() {
   const [articles, setArticles] = useState([]);
 
